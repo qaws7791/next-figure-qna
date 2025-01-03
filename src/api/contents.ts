b60a2080514690b0d9f5ed686b396ed1159a5a21
qaws7791 <qaws7791@gmail.com>
2025-01-03 22:46:10 +0900

    feat: 콘텐츠 조회 API 및 단일 콘텐츠 조회 기능 추가

--- src/api/contents.ts
+++ src/api/contents.ts
@@ -1,4 +1,10 @@
+import httpClient from "@/api/http-client";
+
 type AskQuestionResponseType = {
+  id: number;
+};
+
+type ContentResponseType = {
   id: number;
   userId: string;
   question: string;
@@ -16,8 +22,15 @@ type AskQuestionResponseType = {
   created_at: string;
 };
 
+type ContentsResponseType = {
+  id: number;
+  userId: string;
+  question: string;
+  created_at: string;
+}[];
+
 export async function askQuestion({ inputText }: { inputText: string }) {
-  const response = await fetch("/api/ask-question", {
+  const response = await httpClient.post("ask-question", {
     method: "POST",
     body: JSON.stringify({ question: inputText }),
   });
@@ -25,5 +38,32 @@ export async function askQuestion({ inputText }: { inputText: string }) {
     throw new Error("API 요청이 실패했습니다.");
   }
   const data = (await response.json()) as AskQuestionResponseType;
+  return data;
+}
+
+export async function getContents({
+  page = 1,
+  limit = 10,
+}: {
+  page?: number;
+  limit?: number;
+}) {
+  const searchParams = new URLSearchParams();
+  searchParams.set("page", String(page));
+  searchParams.set("limit", String(limit));
+  const response = await httpClient.get(`answers?${searchParams.toString()}`);
+  if (!response.ok) {
+    throw new Error("API 요청이 실패했습니다.");
+  }
+  const data = (await response.json()) as ContentsResponseType;
+  return data;
+}
+
+export async function getContent({ id }: { id: string }) {
+  const response = await httpClient.get(`answers/${id}`);
+  if (!response.ok) {
+    throw new Error("API 요청이 실패했습니다.");
+  }
+  const data = (await response.json()) as ContentResponseType;
   return data;
 }
