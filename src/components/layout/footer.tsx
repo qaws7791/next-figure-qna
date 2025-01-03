863c123a225952e3ebc69e768c050810a68960ff
qaws7791 <qaws7791@gmail.com>
2025-01-03 22:47:36 +0900

    style: 푸터 컴포넌트의 패딩을 조정

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto max-w-4xl text-center mx-auto px-4 py-20">
+    <footer className="mt-auto max-w-4xl text-center mx-auto px-4 py-8">
       <p className="text-xs text-gray-600">© 2024 FigureQnA.</p>
     </footer>
   );
