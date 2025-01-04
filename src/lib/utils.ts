e6a7919d2efb7d256bbc6a4699019dd265db12c8
qaws7791 <qaws7791@gmail.com>
2025-01-04 22:53:40 +0900

    feat: range 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function range(start: number, end: number, step = 1) {
+  return Array.from(
+    { length: (end - start) / step + 1 },
+    (_, i) => start + i * step
+  );
 }
