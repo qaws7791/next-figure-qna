b7b7ed8c510c45ce028337a3f1d73bbd9ad43d70
qaws7791 <qaws7791@gmail.com>
2025-01-04 22:53:46 +0900

    feat: PageContainer 컴포넌트 추가

---
+++ src/components/layout/page-container.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export default function PageContainer({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col pt-4 pl-4 pr-4 pb-4  max-w-screen-lg mx-auto",
+        className
+      )}
+      {...props}
+    />
+  );
+}
