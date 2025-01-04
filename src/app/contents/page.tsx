cfa61f7f9c553a5440bc271d67ab9e0c73618ecc
qaws7791 <qaws7791@gmail.com>
2025-01-04 22:55:23 +0900

    feat: ContentsPage에 PageContainer 추가하여 레이아웃 개선

--- src/app/contents/page.tsx
+++ src/app/contents/page.tsx
@@ -1,11 +1,12 @@
 import ContentsClient from "@/app/contents/contents-client";
+import PageContainer from "@/components/layout/page-container";
 
 import React from "react";
 
 export default function ContentsPage() {
   return (
-    <div>
+    <PageContainer>
       <ContentsClient />
-    </div>
+    </PageContainer>
   );
 }
