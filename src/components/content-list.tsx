a79a98a7bd675732c1ec080f1ce20032376756ad
qaws7791 <qaws7791@gmail.com>
2025-02-04 23:54:23 +0900

    feat: ContentList 컴포넌트에 데이터 prop 추가 및 스타일 수정

--- src/components/content-list.tsx
+++ src/components/content-list.tsx
@@ -1,22 +1,25 @@
 "use client";
-import { useContentInfiniteQuery } from "@/hooks/queries/contents";
 import Link from "next/link";
 
-export default function ContentList() {
-  const { data } = useContentInfiniteQuery();
+interface ContentListProps {
+  data: Array<{
+    id: number;
+    question: string;
+    created_at: string;
+  }>;
+}
 
-  const allItems = data?.pages.flat();
-
+export default function ContentList({ data }: ContentListProps) {
   return (
     <section>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        {allItems.map((item) => (
+        {data.map((item) => (
           <Link
             href={`/contents?id=${item.id}`}
-            className="p-4 border border-zinc-200 rounded-2xl hover:shadow-md transition-shadow min-h-[162px]"
+            className="p-4 border border-zinc-200 rounded-2xl hover:shadow-md transition-shadow min-h-[169px]"
             key={item.id}
           >
-            <p className="text-2xl font-medium line-clamp-5 md:line-clamp-4">
+            <p className="text-lg font-medium line-clamp-6 leading-tight md:line-clamp-4">
               {item.question}
             </p>
 
